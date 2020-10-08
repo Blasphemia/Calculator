f4b41b2ccb3294bf91260af55425147cd44878e7
deathka1990 <deathka1990@gmail.com>
2020-10-08 16:24:41 +0300

    Add project files.

---
+++ Calculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace Calculator
+{
+	class Calculator
+	{
+		public static double DoOperation(double num1, double num2, string op)
+		{
+			double res = double.NaN;
+			switch (op)
+			{
+				case "a":
+					res = num1 + num2;
+					break;
+
+				case "s":
+					res = num1 - num2;
+					break;
+
+				case "m":
+					res = num1 * num2;
+					break;
+
+				case "d":
+					res = num1 / num2;
+					break;
+
+				default: break;
+			}
+			return res;
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Calculator
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			bool endApp = false;
+			Console.WriteLine("Console Calculator in C#\r");
+			Console.WriteLine("------------------------\n");
+
+			while (!endApp)
+			{
+				string numInput1 = "";
+				string numInput2 = "";
+				double res = 0;
+				double cleanNum1; double cleanNum2;
+
+				Console.WriteLine("Type a number, and then press Enter");
+				numInput1 = Console.ReadLine();
+
+				while (!double.TryParse(numInput1, out cleanNum1))
+				{
+					Console.Write("This is not valid input. Please enter an integer value: ");
+					numInput1 = Console.ReadLine();
+				}
+
+				Console.WriteLine("Type a number, and then press Enter");
+				numInput2 = Console.ReadLine();
+
+				while (!double.TryParse(numInput2, out cleanNum2))
+				{
+					Console.Write("This is not valid input. Please enter an integer value: ");
+					numInput2 = Console.ReadLine();
+				}
+
+
+
+
+
+
+				Console.WriteLine("Choose an option from the following list:");
+				Console.WriteLine("\t a - Add");
+				Console.WriteLine("\t s - Subtract");
+				Console.WriteLine("\t m - Multiply");
+				Console.WriteLine("\t d - Divide");
+				Console.Write("Your option? ");
+
+				string op = Console.ReadLine();
+
+				while (op != "a" && op != "s" && op != "m" && op != "d") 
+				{
+					Console.Write("Please, choose a correct operation: ");
+					op = Console.ReadLine();
+				}
+					
+			
+				
+
+				try
+				{
+					res = Calculator.DoOperation(cleanNum1, cleanNum2, op);
+					if (double.IsNaN(res))
+					{
+						Console.WriteLine("This operation will result in a mathematical error.\n");
+					}
+					else Console.WriteLine("Your result: {0:0.##}\n", res);
+				}
+				catch (Exception e)
+				{
+					Console.WriteLine("Oh no! An exception occurred trying to do the math.\n - Details: " + e.Message);
+				}
+
+				Console.Write("Do you want to continue? \n Press 'n' to close the app, or press 'y' to continue: ");
+				string key = Console.ReadLine();
+
+
+				do
+				{
+					if (key == "n")
+					{
+						endApp = true; break;
+					}
+					else if (key == "y")
+					{
+						endApp = false; break;
+					}
+					else
+					{
+						Console.Write("Please, press 'n' to close the app, or press 'y' to continue: ");
+						key = Console.ReadLine();
+					}
+
+
+				}
+				while (key != "n" || key != "y");
+
+
+				
+				Console.WriteLine("\n");
+			}
+
+
+
+			return;
+		}
+
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
